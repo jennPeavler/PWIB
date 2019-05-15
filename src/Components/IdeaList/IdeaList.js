1cd3f5ee88225bbefc7059ecde86a1798fbcf09c
Jennifer Peavler <jenn@crafted.education>
2019-05-14 22:40:03 -0600

    alphabetize ideas

--- src/Components/IdeaList/IdeaList.js
+++ src/Components/IdeaList/IdeaList.js
@@ -4,7 +4,7 @@ import './IdeaList.scss';
 import IdeaCard from '../IdeaCard/IdeaCard';
 
 const IdeaList = ({ideas}) => {
-	const ideaList = ideas.map(idea => <IdeaCard key={idea.id} idea={idea} />);
+	const ideaList = ideas.sort((a, b) => a.title > b.title ? 1 : a.title < b.title ? -1 : 0).map(idea => <IdeaCard key={idea.id} idea={idea} />);
 	return (
 		<div className='idea-list'>
 			{ideaList}
